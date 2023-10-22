9f551ee9587f3f74d95f1e06c3bfb8f6f3ebb378
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-10-21 20:09:21 -0400

    added/fixed routes

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -3,10 +3,17 @@ const path = require("path");
 
 router.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname, "../views/index.hbs"));
-    res.render("../Views/landing.hbs");
+    res.render("../views/landing.hbs");
 });
 router.get("/signup", (req, res) => {
-    res.render("../Views/signUp.hbs");
+    res.render("../views/loginSignUP.hbs");
 });
+router.get("/dashboard", (req, res) => {
+    res.render("../views/dashboard.hbs");
+})
+router.get("/calc", (req, res) => {
+    res.render("../views/savings-calc.hbs")
+})
+
 
 module.exports = router;
