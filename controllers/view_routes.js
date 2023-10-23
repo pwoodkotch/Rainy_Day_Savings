1528bd6239c3bfd692f1444e4ec2e385098d8b1a
BittersweetNSour <115330642+BittersweetNSour@users.noreply.github.com>
2023-10-23 11:03:47 -0400

    Dashboard is now dynamic

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -9,7 +9,57 @@ router.get("/signup", (req, res) => {
     res.render("../views/loginSignUP.hbs");
 });
 router.get("/dashboard", (req, res) => {
-    res.render("../views/dashboard.hbs");
+    //Get info from database
+    //Convert information from database to comprehensible json
+    let saveAmountA = 317;
+    let targetAmountA = 500;
+    let saveAmountB = 267;
+    let targetAmountB = 900;
+    let completionPercentA = Math.floor(100 * saveAmountA/targetAmountA)
+    let completionPercentB = Math.floor(100 * saveAmountB/targetAmountB)
+    const savingsGoal = {
+        firstGoal: {
+            name: "Bike Fund",
+            startDate: "Today",
+            timeOfUpdate: "3h ago",
+            saved: saveAmountA,
+            target: targetAmountA,
+            completion: completionPercentA
+        },
+        secondGoal: {
+            name: "New Computer",
+            startDate: "Today",
+            timeOfUpdate: "9h ago",
+            saved: saveAmountB,
+            target: targetAmountB,
+            completion: completionPercentB
+        },
+        thirdGoal: {
+            name: "New Computer",
+            startDate: "Today",
+            timeOfUpdate: "9h ago",
+            saved: saveAmountB,
+            target: targetAmountB,
+            completion: completionPercentB
+        },
+        backupA: {
+            name: "Rent Backup",
+            startDate: "Today",
+            timeOfUpdate: "2h ago",
+            saved: 700
+        },
+        rainyDayFund: {
+            name: "For a Rainy Day",
+            startDate: "Today",
+            timeOfUpdate: "3h ago",
+            saved: 900,
+            completion: 100
+        }
+    }
+    res.render("../views/dashboard.hbs", {
+        goal: savingsGoal,
+        user: req.user
+    });
 })
 router.get("/calc", (req, res) => {
     res.render("../views/savings-calc.hbs")
