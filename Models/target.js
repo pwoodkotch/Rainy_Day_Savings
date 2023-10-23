62a93bae2927b373730c978e460819fb50f7c40b
b0n3yard <namesake39@gmail.com>
2023-10-23 15:11:05 -0400

    end of monday

--- Models/target.js
+++ Models/target.js
@@ -7,7 +7,25 @@ Target.init({
     target_amount:{
         type:DataTypes.INTEGER,
         allowNull: false
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    started:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    saved:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    },
+    completion:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+
     }
+
 },{
     modelName:'target',
     sequelize: db,
