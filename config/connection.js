2b344c51c9069f81b42af8b2ca3c2066fc82709e
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-10-24 11:18:05 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
 const is_prod = process.env.PORT
-const sequelize = is_prod ? process.env.JAWSDB_URL : new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
+const sequelize = is_prod ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: "mysql", 
     logging: false,
